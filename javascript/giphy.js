e8ebb873637bf527914ae0739852212a59973483
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-05-02 18:44:59 -0400

    Buttons Rendering Properly, Accessing API correctly and adds new buttons upon input, those buttons also access the API

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,74 +1,70 @@
-// access API
-// display rating
-//display static image
-//on click of image it begins to move
-
-
 // array that holds the already defined buttons on screen in the gif-buttons id
-var food = ["bacon", "cheeseburger", "guacamole", "pizza", "chicken+nuggets", "pretzels", "salad", "milkshake"];
+var food = ["Bacon", "Burritos", "Guacamole", "Grilled Cheese", "Pizza", "Pancakes"];
 console.log(food);
 
-// Public API Key
-var key = "dc6zaTOxFJmzC";
 
- // URL that gets called in the ajax function that is requesting info from the Giphy API
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+food+"&api_key="+key+"&limit=10";
-// ajax function that requests information from the Giphy API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-   
-            // Function for displaying movie data
-      function renderButtons() {
+// function that creates the gif buttons 
+function renderButtons() {
+  // remove everything from the gif button div
+  $("#gif-buttons").empty();
+  // Loops through the array of pre-defined food variable
+  for (var i = 0; i < food.length; i++) {
+    // Generates buttons for each food in the food variable array
+    var button = $("<button>");
+    // Adds a class of gifButton to our buttons
+    button.addClass("gifButton");
+    // Added a data-attribute
+    button.attr("data-name", food[i]);
+    // Provided the initial button text
+    button.text(food[i]);
+    // Added the button to the div with id gif buttons
+    $("#gif-buttons").append(button);
+  }
 
-        // Deletes the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#gif-buttons").empty();
-        // Loops through the array of movies
-        for (var i = 0; i < food.length; i++) {
+  // On Click function for each gif button that pushes that value into the queryURL variable
+$(".gifButton").click(function(){
+  $("#food-gifs").empty();
+var gifClicked = $(this).attr("data-name");
+console.log("Gif Button was clicked! I have a data-name of: " + gifClicked);
+  // URL that gets called in the ajax function that is requesting info from the Giphy API
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        gifClicked + "&api_key=dc6zaTOxFJmzC&limit=10";
+  console.log(queryURL);
+  // ajax function that requests information from the Giphy API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    var gif = response.data;
+      console.log(gif);
 
-          // Then dynamicaly generates buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adds a class of movie to our button
-          a.addClass("gifButton");
-          // Added a data-attribute
-          a.attr("data-name", food[i]);
-          // Provided the initial button text
-          a.text(food[i]);
-          // Added the button to the buttons-view div
-          $("#gif-buttons").append(a);
-        }
+      for (i = 0; i<gif.length; i++){
+        var gifImg = $("<img>");
+
+        gifImg.attr("src", gif[i].images.fixed_height.url);
+
+        $("#food-gifs").prepend(gifImg);
       }
-
-      // This function handles events where the add movie button is clicked
-      $("#add-button").on("click", function() {
-        console.log("----------------------------------");
-        console.log("Add button was clicked!!!!!!");
-        console.log("-----------------------------------");
-        // This line of code will grab the input from the textbox
-        var foods = $("#gif-input").val().trim();
-
-        // The movie from the textbox is then added to our array
-        food.push(foods);
-
-      });
-
-      // Adding click event listeners to all elements with a class of "movie"
-      $("#gif-buttons").click(function(){
-      
-      console.log("Gif Button was clicked!!!!!!!!!!!");
-      });
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
-
-// $("food-gifs").prepend();
+    });
+});
+}
+renderButtons();
 
 
 
+// This function handles events where the add movie button is clicked
+$("#add-button").click(function(){
+  event.preventDefault();
+  console.log("----------------------------------");
+  console.log("Add button was clicked!!!!!!");
+  console.log("-----------------------------------");
+  // This line of code will grab the input from the textbox
+  var gifInput = $("#gif-input").val().trim();
+  // The movie from the textbox is then added to our array
+  food.push(gifInput);
+
+  console.log(food);
+  renderButtons();
+});
 
 
- });
-    
